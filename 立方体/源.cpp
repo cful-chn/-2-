d963d482c49a2bf2fc84ae06d7e306849ae97060
cful <103881568+cful-chn@users.noreply.github.com>
2022-12-02 21:11:42 +0100

    立方体

---
+++ 立方体/源.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class dian
+{
+public:
+	void schreibdianX(int a)
+	{
+		dianX = a;
+	}
+
+	void getdianY(int b)
+	{
+		dianY = b;
+	}
+	int lesendianX()
+	{
+		cout << dianX << endl;
+	}
+	int lesendianY()
+	{
+		cout << dianY << endl;
+	}
+private:
+	int dianX;
+	int dianY;
+};
+class guanxi
+{
+
+
+
+
+//Êä³ö°ë¾¶
+private:
+	int r;
+	dian yuanxin;
+	
+		
+};
+
+int main() {
+
+	system("pause");
+	return 0;
+}
